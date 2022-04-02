b82933c78f69b6a3d2eeb0c17e3889ce2a7597e7
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2022-04-02 13:05:50 +0500

    3) задали размер, позицию окна и загаловок

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <GL/freeglut.h>			// добавили библиотеку
+#include <GL/freeglut.h>								// добавили библиотеку
 
 int main(int argc, char** argv) {
 	
-	glutInit(&argc, argv);			// инициализировали glut
+	glutInit(&argc, argv);								// инициализировали glut
+
 
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);		// настрили опции: двойную буферизацию и буфер цвета
 
 
+	glutInitWindowSize(1024, 768);						// установили размер окна 1024 на 768 пикселей
+	glutInitWindowPosition(100, 100);					// установили позицию на экране
+	glutCreateWindow("Tutorial 01");					// заголовок окна
+	
 }
