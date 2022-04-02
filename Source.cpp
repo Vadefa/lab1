f56773fa2fec37059e56ccd5a46de773b48d1c2c
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2022-04-02 13:03:04 +0500

    2) настроили опции отображения

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-#include <GL/freeglut.h>	// добавили библиотеку
+#include <GL/freeglut.h>			// добавили библиотеку
 
 int main(int argc, char** argv) {
 	
-	glutInit(&argc, argv);	// инициализировали glut
+	glutInit(&argc, argv);			// инициализировали glut
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);		// настрили опции: двойную буферизацию и буфер цвета
+
+
 }
