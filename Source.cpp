afb0d84dda0512de4ddd63eb16a0dc51d9218269
Вадим Сухинин <suhinin.vadim2002@yandex.ru>
2022-04-30 15:51:35 +0500

    первоначальный вариант

--- Source.cpp
+++ Source.cpp
@@ -5,35 +5,42 @@
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint createShaderProgram() {
-	const char* vshaderSource =
-		"#version 430 \n"
+	const char* vshaderSource =									//1) declaring character string vshaderSource: the vertex shader
+		"#version 430 \n"										//15) indicating OpenGL version
 		"void main(void) \n"
-		"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
-	const char* fshaderSource =
+		"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";			//16) this variable sets vertex's coordinate position in 3D space (to the origin location); and it being sent next to the pipeline
+	const char* fshaderSource =									//1) declaring character string fshaderSource: the fragment shader
 		"#version 430 \n"
-		"out vec4 color; \n"
+		"out vec4 color; \n"									//17) the "out" tag indicates that the variable color is an output
 		"void main(void) \n"
 		"{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
-	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(vShader, 1, &vshaderSource, NULL);
-	glShaderSource(fShader, 1, &fshaderSource, NULL);
-	glCompileShader(vShader);
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);			//2) generating a shader of type GL_VERTEX_SHADER, initially empty, and returning its id to vShader
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);		//2) generating a shader of type GL_FRAGMENT_SHADER, initially empty, and returning its id to fShader
+	glShaderSource(vShader, 1, &vshaderSource, NULL);			//3) loading the GLSL code from strings vshaderSource and fshaderSource into the empty shader objects
+	glShaderSource(fShader, 1, &fshaderSource, NULL);			/* parameters of the function: 
+																a) shader object in which to store the shader
+																b) the number of strings in the shader source code
+																c) an array of pointers to strings containing the source code
+																d) an additional parameter we aren't using now
+																*/
+	glCompileShader(vShader);									//4) compiling both shaders
 	glCompileShader(fShader);
-	GLuint vfProgram = glCreateProgram();
-	glAttachShader(vfProgram, vShader);
+	GLuint vfProgram = glCreateProgram();						//5) creating a program obect and saving the integer ID that points to it in the vfProgram
+	glAttachShader(vfProgram, vShader);							//6) attaching shaders to the program object
 	glAttachShader(vfProgram, fShader);
-	glLinkProgram(vfProgram);
+	glLinkProgram(vfProgram);									//7) requesting that the GLSL compiler ensure that shaders are compatible
 	return vfProgram;
 }
+
 void init(GLFWwindow* window) {
 	renderingProgram = createShaderProgram();
-	glGenVertexArrays(numVAOs, vao);
-	glBindVertexArray(vao[0]);
+	glGenVertexArrays(numVAOs, vao);							//18) we will consider it later. Just to know, OpenGL requires that at least one vertex array object be created whenever...
+	glBindVertexArray(vao[0]);									//.. shaders are being used, even if we need to display only one point that does not require any buffers.
 }
 void display(GLFWwindow* window, double currentTime) {
-	glUseProgram(renderingProgram);
-	glDrawArrays(GL_POINTS, 0, 1);
+	glUseProgram(renderingProgram);														//13) the function loads the program containing the two complied shaders into the OpenGL pipeline stages (onto the GPU)
+	glPointSize(30.0f);																	//19) in the rasterizer default points size is 1 pixel. Here we set it to 30 pixels.
+	glDrawArrays(GL_POINTS, 0, 1);														//14) initiating pipline processing - displaying a single point
 }
 
 int main(void) {
